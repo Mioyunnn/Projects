55a861639504f61948911d02e1a516d75ec5abda
Mioyunnn <lion065030@gmail.com>
2024-07-15 21:07:17 +0800

    Add links

---
+++ script.js
@@ -0,0 +1,96 @@
+const icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "dark theme icon/moon.png";
+    }
+    else{
+        icon.src = "dark theme icon/sun.png";
+    }
+}
+//連結們
+document.getElementById('project-1').addEventListener('click', function() {
+    window.location.href = '/Product Page Design/index.html';
+});
+document.getElementById('project-2').addEventListener('click', function() {
+    window.location.href = '/Image Search Engine/index.html';
+});
+document.getElementById('project-3').addEventListener('click', function() {
+    window.location.href = '/Random Password Generator/index.html';
+});
+document.getElementById('project-4').addEventListener('click', function() {
+    window.location.href = '/Voice Converter/index.html';
+});
+document.getElementById('project-5').addEventListener('click', function() {
+    window.location.href = '/Toast Notification/index.html';
+});
+document.getElementById('project-6').addEventListener('click', function() {
+    window.location.href = '/Subscribe Form to Google Sheet/index.html';
+});
+document.getElementById('project-7').addEventListener('click', function() {
+    window.location.href = '/Stop watch/index.html';
+});
+document.getElementById('project-8').addEventListener('click', function() {
+    window.location.href = '/Quote Generator/index.html';
+});
+document.getElementById('project-9').addEventListener('click', function() {
+    window.location.href = '/QR Code Generator/index.html';
+});
+document.getElementById('project-10').addEventListener('click', function() {
+    window.location.href = '/Popup/index.html';
+});
+document.getElementById('project-11').addEventListener('click', function() {
+    window.location.href = '/Password toggle/index.html';
+});
+document.getElementById('project-12').addEventListener('click', function() {
+    window.location.href = '/Password Strength Detector/index.html';
+});
+document.getElementById('project-13').addEventListener('click', function() {
+    window.location.href = '/Notes App/index.html';
+});
+document.getElementById('project-14').addEventListener('click', function() {
+    window.location.href = '/Music Player/index.html';
+});
+document.getElementById('project-15').addEventListener('click', function() {
+    window.location.href = '/Mini Calendar/index.html';
+});
+document.getElementById('project-16').addEventListener('click', function() {
+    window.location.href = '/Image Background Transition/index.html';
+});
+document.getElementById('project-17').addEventListener('click', function() {
+    window.location.href = '/Horizontal Scroll Image Gallery/index.html';
+});
+document.getElementById('project-18').addEventListener('click', function() {
+    window.location.href = '/Form Validation/index.html';
+});
+document.getElementById('project-19').addEventListener('click', function() {
+    window.location.href = '/Select Menu/index.html';
+});
+document.getElementById('project-20').addEventListener('click', function() {
+    window.location.href = '/Coming Soon Page/index.html';
+});
+document.getElementById('project-21').addEventListener('click', function() {
+    window.location.href = '/Calculator/index.html';
+});
+document.getElementById('project-22').addEventListener('click', function() {
+    window.location.href = '/Age Calculator/index.html';
+});
+document.getElementById('project-23').addEventListener('click', function() {
+    window.location.href = '/Circular Progress Bar/index.html';
+});
+document.getElementById('project-24').addEventListener('click', function() {
+    window.location.href = '/Cryptocurrency/index.html';
+});
+document.getElementById('project-25').addEventListener('click', function() {
+    window.location.href = '/Digital Clock/index.html';
+});
+document.getElementById('project-26').addEventListener('click', function() {
+    window.location.href = '/Drag & Drop/index.html';
+});
+document.getElementById('project-27').addEventListener('click', function() {
+    window.location.href = '/Quiz App/index.html';
+});
+document.getElementById('project-28').addEventListener('click', function() {
+    window.location.href = '/To-do List App/index.html';
+});
