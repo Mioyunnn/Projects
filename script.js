526b6ddb65a1f4b7b33c015450c673ddd4f71254
Mioyunnn <lion065030@gmail.com>
2024-07-16 22:09:33 +0800

    test success

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ icon.onclick = function(){
 }
 //連結們
 document.getElementById('project-1').addEventListener('click', function() {
-    window.location.href = 'Product Page Design/ch1.html';
+    window.location.href = 'Product Page Design/index.html';
 });
 document.getElementById('project-2').addEventListener('click', function() {
     window.location.href = 'Image Search Engine/index.html';
@@ -26,73 +26,73 @@ document.getElementById('project-5').addEventListener('click', function() {
     window.location.href = 'Toast Notification/index.html';
 });
 document.getElementById('project-6').addEventListener('click', function() {
-    window.location.href = '/Subscribe Form to Google Sheet/index.html';
+    window.location.href = 'Subscribe Form to Google Sheet/index.html';
 });
 document.getElementById('project-7').addEventListener('click', function() {
-    window.location.href = '/Stop watch/index.html';
+    window.location.href = 'Stop watch/index.html';
 });
 document.getElementById('project-8').addEventListener('click', function() {
-    window.location.href = '/Quote Generator/index.html';
+    window.location.href = 'Quote Generator/index.html';
 });
 document.getElementById('project-9').addEventListener('click', function() {
-    window.location.href = '/QR Code Generator/index.html';
+    window.location.href = 'QR Code Generator/index.html';
 });
 document.getElementById('project-10').addEventListener('click', function() {
-    window.location.href = '/Popup/index.html';
+    window.location.href = 'Popup/index.html';
 });
 document.getElementById('project-11').addEventListener('click', function() {
-    window.location.href = '/Password toggle/index.html';
+    window.location.href = 'Password toggle/index.html';
 });
 document.getElementById('project-12').addEventListener('click', function() {
-    window.location.href = '/Password Strength Detector/index.html';
+    window.location.href = 'Password Strength Detector/index.html';
 });
 document.getElementById('project-13').addEventListener('click', function() {
-    window.location.href = '/Notes App/index.html';
+    window.location.href = 'Notes App/index.html';
 });
 document.getElementById('project-14').addEventListener('click', function() {
-    window.location.href = '/Music Player/index.html';
+    window.location.href = 'Music Player/index.html';
 });
 document.getElementById('project-15').addEventListener('click', function() {
-    window.location.href = '/Mini Calendar/index.html';
+    window.location.href = 'Mini Calendar/index.html';
 });
 document.getElementById('project-16').addEventListener('click', function() {
-    window.location.href = '/Image Background Transition/index.html';
+    window.location.href = 'Image Background Transition/index.html';
 });
 document.getElementById('project-17').addEventListener('click', function() {
-    window.location.href = '/Horizontal Scroll Image Gallery/index.html';
+    window.location.href = 'Horizontal Scroll Image Gallery/index.html';
 });
 document.getElementById('project-18').addEventListener('click', function() {
-    window.location.href = '/Form Validation/index.html';
+    window.location.href = 'Form Validation/index.html';
 });
 document.getElementById('project-19').addEventListener('click', function() {
-    window.location.href = '/Select Menu/index.html';
+    window.location.href = 'Select Menu/index.html';
 });
 document.getElementById('project-20').addEventListener('click', function() {
-    window.location.href = '/Coming Soon Page/index.html';
+    window.location.href = 'Coming Soon Page/index.html';
 });
 document.getElementById('project-21').addEventListener('click', function() {
-    window.location.href = '/Calculator/index.html';
+    window.location.href = 'Calculator/index.html';
 });
 document.getElementById('project-22').addEventListener('click', function() {
-    window.location.href = '/Age Calculator/index.html';
+    window.location.href = 'Age Calculator/index.html';
 });
 document.getElementById('project-23').addEventListener('click', function() {
-    window.location.href = '/Circular Progress Bar/index.html';
+    window.location.href = 'Circular Progress Bar/index.html';
 });
 document.getElementById('project-24').addEventListener('click', function() {
-    window.location.href = '/Cryptocurrency/index.html';
+    window.location.href = 'Cryptocurrency/index.html';
 });
 document.getElementById('project-25').addEventListener('click', function() {
-    window.location.href = '/Digital Clock/index.html';
+    window.location.href = 'Digital Clock/index.html';
 });
 document.getElementById('project-26').addEventListener('click', function() {
-    window.location.href = '/Drag & Drop/index.html';
+    window.location.href = 'Drag & Drop/index.html';
 });
 document.getElementById('project-27').addEventListener('click', function() {
-    window.location.href = '/Quiz App/index.html';
+    window.location.href = 'Quiz App/index.html';
 });
 document.getElementById('project-28').addEventListener('click', function() {
-    window.location.href = '/To-do List App/index.html';
+    window.location.href = 'To-do List App/index.html';
 });
 
 let dates = ["2024 / 7 / 15", "2024 / 7 / 15", "2024 / 7 / 16",
